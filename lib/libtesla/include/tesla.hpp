7137dbcec929e764bf373c6bdf791f29d66bf6ab
Efosamark <efosamark@gmail.com>
2023-09-10 21:44:48 +0300

    Fixed build issue [2]

--- lib/libtesla/include/tesla.hpp
+++ lib/libtesla/include/tesla.hpp
@@ -2424,7 +2424,7 @@ namespace tsl {
                         break;
                     case tsl::PredefinedColors::DefaultText:
                     default:
-                        this->m_color = tsl::Color::ColorText;
+                        this->m_color = tsl::style::color::ColorText;
                         break;
                 }
                 this->m_maxWidth = 0;
