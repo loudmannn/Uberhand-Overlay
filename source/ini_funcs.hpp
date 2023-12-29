2afcdff1dc5cdb7d7169a026a02ad3f491c038b3
i3sey <deniskedrovsky@mail.ru>
2023-12-29 18:28:55 +0500

    added check for ?per_page=1

--- source/ini_funcs.hpp
+++ source/ini_funcs.hpp
@@ -132,6 +132,9 @@ PackageHeader getPackageHeaderFromIni(const std::string& filePath) {
                 // Value not enclosed in quotes
                 packageHeader.github = strLine.substr(repoPos, endPos - repoPos);
             }
+            if (packageHeader.github.substr(packageHeader.github.length() - 11, 11) != "?per_page=1") {
+                packageHeader.github += "?per_page=1";
+            }
         }
     }
 
